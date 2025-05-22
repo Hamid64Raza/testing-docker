12bfbbad58b1d8fa462eae7eeef0046fe5497669
Hamid64Raza <hamidraza.raza@gamil.com>
2025-05-22 10:17:43 +0530

    change in hello

--- src/main/java/com/demo/controller/HelloWorld.java
+++ src/main/java/com/demo/controller/HelloWorld.java
@@ -1,5 +1,12 @@
 package com.demo.controller;
 
-public class HelloWorld {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HelloWorld {
+	@RequestMapping("/message")
+  public String getMessage() {
+	  return "Hello world";
+  }
 }
