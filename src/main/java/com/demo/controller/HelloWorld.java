1b48c3cd748683c11c219f75d60c56540b88e773
Hamid Raza <112300409+Hamid64Raza@users.noreply.github.com>
2025-05-24 17:45:16 +0530

    Update HelloWorld.java

--- src/main/java/com/demo/controller/HelloWorld.java
+++ src/main/java/com/demo/controller/HelloWorld.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
   @GetMapping("/message")
   public String getMessage() {
-	  return "Hello world";
+	  return "Hello world jenkins";
   }
 }
